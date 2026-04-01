11f58d14e1ea78eaf4e237683c49b9c149413db1
JJRoc <214893617+ChaosMaster8673@users.noreply.github.com>
2026-04-01 17:54:52 -0400

    work?

--- src/main/java/com/academics/undertone/item/ModCreativeModeTabs.java
+++ src/main/java/com/academics/undertone/item/ModCreativeModeTabs.java
@@ -24,6 +24,10 @@ public class ModCreativeModeTabs {
                             output.accept(ModBlocks.ZINC_ORE);
                             output.accept(ModBlocks.ZINC_BLOCK);
                             output.accept(ModItems.BLOCK_SWAPPER);
+                            output.accept(ModItems.FIRSTDUNGEON_CHESTPLATE);
+                            output.accept(ModItems.FIRSTDUNEGON_BOOTS);
+                            output.accept(ModItems.FIRSTDUNEGON_HELMET);
+                            output.accept(ModItems.FIRSTDUNGEON_LEGGINS);
                         })
                         .build());
         public static void register(IEventBus bus) { // this is to allow the game to register our creative mode tab at the appropriate time during mod loading. This should always be called from the constructor of your main mod class, and the bus parameter should be the mod event bus passed into the constructor.
