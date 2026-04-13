afed11ef561fd00952bfebe8e0e5585935443db4
Chaosmaster8673 <--global>
2026-04-13 09:14:49 -0400

    I was here

--- src/main/java/com/academics/undertone/entity/client/Rudahh/RudahhRenderer.java
+++ src/main/java/com/academics/undertone/entity/client/Rudahh/RudahhRenderer.java
@@ -7,7 +7,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
-
+//JJ was here.
 public class RudahhRenderer extends MobRenderer<RudahhEntity, RudahhModel<RudahhEntity>> {
 
     public RudahhRenderer(EntityRendererProvider.Context context) {
