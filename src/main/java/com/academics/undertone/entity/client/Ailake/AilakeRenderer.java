47061ae81f589c73e838042dddcb84e441a5d52c
afterglow79 <cakeby11@gmail.com>
2026-04-13 09:13:12 -0400

    add TODO to AilakeRenderer.java -- still have to set up scale properly, at a placeholder of 10x10x10f

--- src/main/java/com/academics/undertone/entity/client/Ailake/AilakeRenderer.java
+++ src/main/java/com/academics/undertone/entity/client/Ailake/AilakeRenderer.java
@@ -24,7 +24,7 @@ public class AilakeRenderer extends MobRenderer<AilakeEntity, AilakeModel<Ailake
         if(entity.isBaby()){
             poseStack.scale(0.5f, 1.0f, 0.5f); // Scale down the model for baby entities
         } else {
-            poseStack.scale(1.5f, 1.5f, 1.5f); // Normal scale for adult entities
+            poseStack.scale(10f, 10f, 10f); // Normal scale for adult entities TODO scale right just like hitbox
         }
 
         super.render(entity, entityYaw, partialTicks, poseStack, bufferSource, packedLight);
